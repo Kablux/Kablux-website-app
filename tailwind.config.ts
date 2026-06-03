db48520c908c0362c451d449edb145605f588563
segun abiodun <abiodun_segun10@yahoo.com>
2026-06-03 15:53:14 +0200

    project set up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,54 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+ theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FEB914",
+        grey: "#606060",
+        tertiary: "#4B5563",
+      },
+
+      screens: {
+        xx: "400px",
+        xs: "480px",
+        ss: "596px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1240px",
+        xl: "1440px",
+        "2xl": "1512px",
+      },
+      fontSize: {
+        base: "1rem",
+        sm: [
+          "1.5rem",
+          {
+            fontWeight: "500",
+          },
+        ],
+        md: [
+          "2.25rem",
+          {
+            fontWeight: "700",
+          },
+        ],
+        lg: [
+          "3rem",
+          {
+            fontWeight: "700",
+          },
+        ],
+        xl: [
+          "3.75rem",
+          {
+            fontWeight: "700",
+          },
+        ],
       },
     },
   },
   plugins: [],
 };
 export default config;
+
